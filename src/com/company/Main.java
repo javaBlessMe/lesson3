5f81d9534055528de93a619963376948ce068497
javaBlessMe <57236195+javaBlessMe@users.noreply.github.com>
2020-01-23 22:01:45 +0300

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javafx.css.Match;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+    }
+}
